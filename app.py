1f9c52c86c05d63a10d038729df3350a43033c73
Wilson Chan <wilsonchan54@gmail.com>
2021-11-18 15:15:57 -0500

    Adding form to index.html / create processed.html to show submission / add submit function to receive POST form data from index.html

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        score = request.form['score']
+        adjective = request.form['adjective']
+                #print(first_name, last_name, score, adjective)
+        return render_template('processed.html')
+
 if __name__ == '__main__':
     app.debug = True             # Server reloads after each save/change
     app.run()
