57a69af21312c22b8da6329d59e074eb3ed75c4a
Wilson Chan <wilsonchan54@gmail.com>
2021-11-18 23:46:02 -0500

    Creating database with model / add configuration for deployment with sqlalchemy / move style.css to static folder

--- app.py
+++ app.py
@@ -1,6 +1,46 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+#------------------------------------------------------------#
+# Development/Production Mode
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True             # Server reloads after each save/change
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:****pw goes here****@localhost/mlh-app'        # Development Database
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False     # Warning in console
+
+
+#------------------------------------------------------------#
+
+# Database
+db = SQLAlchemy(app)
+
+
+# Model
+class info_storage(db.Model):
+    __tablename__ = 'person'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(30))
+    score = db.Column(db.Integer)
+    adjective = db.Column(db.String(30))
+
+    def __init__(self, first_name, last_name, score, adjective):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.score = score
+        self.adjective = adjective
+
+#------------------------------------------------------------#
 
 @app.route('/')
 def index():
@@ -16,6 +56,8 @@ def submit():
                 #print(first_name, last_name, score, adjective)
         return render_template('processed.html')
 
+
+#------------------------------------------------------------#
+
 if __name__ == '__main__':
-    app.debug = True             # Server reloads after each save/change
     app.run()
